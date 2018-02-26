8f3a3e75711d5ab08f96923c27e6b14fe46a4910
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-26 15:00:08 -0500

    changed the name of the variable to profilePicture since thats what we are uploading

--- routes/upload-route.js
+++ routes/upload-route.js
@@ -6,11 +6,11 @@ module.exports = function(app) {
         return res.status(400).send('No files were uploaded.');
      
       // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-      let sampleFile = req.files.sampleFile;
-      var filename = req.files.sampleFile.name
+      let profilePicture = req.files.profilePicture;
+      var filename = req.files.profilePicture.name
      console.log(__dirname);
       // Use the mv() method to place the file somewhere on your server
-      sampleFile.mv(__dirname+`_${filename}_image.jpg` , function(err) {
+      profilePicture.mv(__dirname+`_${filename}_image.jpg` , function(err) {
         if (err)
           return res.status(500).send(err.message);
      
