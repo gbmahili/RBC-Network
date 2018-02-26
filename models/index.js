80502e4c1e90a7383e6948b5156d14aad968dccc
gbmahili <georgesmahili@gmail.com>
2018-02-26 07:58:07 -0500

    Removed environment selection

--- models/index.js
+++ models/index.js
@@ -8,11 +8,13 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+var sequelize = new Sequelize(process.env.JAWSDB_URL);
 
 fs
   .readdirSync(__dirname)
