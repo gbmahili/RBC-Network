444d8fad64fe7702e5f00a123c313801ff88e3d3
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-26 13:57:09 -0500

    uploadfile routing is added

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 
-// require("./routes/html-routes.js")(app);
 require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app);
+// require('./routes/upload-route.js')(app)
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
