bd2bd34ddb9b2947b54c3677fe12ae8322c55065
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-03-01 01:00:20 -0500

    checkboxes added dynamically

---
+++ public/js/ajax.js
@@ -0,0 +1,91 @@
+$(document).ready(function(event){
+	
+//============================================
+
+	//GET REQUEST For all the data from profession table
+	//============================================
+
+	//sending a get request
+	// via route /professions
+	// store the response in the data parameter
+	$.get('/professions', function(data){
+	//============================================
+			
+	professionTableIteration(data);
+	//============================================		
+	})// get request closing tag
+
+	
+	//============================================
+
+function professionTableIteration(data){
+
+	//once the get request gets the response 
+	//lets log the data into terminal for tests	
+	
+	
+	data.forEach(function (currentValue, index, array){
+		// console.log(currentValue.occupation);
+		// console.log(currentValue.id);
+
+		var occupation= currentValue.category;
+		var professionid = currentValue.id;
+			
+		htmlHandler(professionid, occupation);
+
+	});
+
+	$(".occupations").click(function(){
+		console.log('working')
+			// class will be shown
+	var idSelected = $(this).prop('value');
+		if ($(this).prop('checked')){
+			// $(`input[type='file'][name='${occupationSelected}']`).removeClass('hidden');
+			$(`.${idSelected}`).removeClass('hidden');
+			// $(".Developer").removeClass("hidden");
+			// console.log(test);
+		} else{
+			$(`.${idSelected}`).addClass('hidden');
+		}
+
+		})
+
+}
+
+function htmlHandler(pk, occupation){
+// PANEL THAT HOLDS ALL THE CHECKBOXES WITH PROFESSION LIST
+//======================================
+
+	var professionList = `
+	<div class="col m4 s12">
+	
+	<input type="checkbox" 
+	class="filled-in occupations" 
+	value="${pk}"
+	id="${occupation}"/>
+
+	<label for="${occupation}">
+		<span style= "color:#F4FCFB;">
+		${occupation}
+		</span>
+	</label>
+	<div class="file-field input-field hidden ${pk}">
+		<div class="btn">
+		  <span>File</span>
+		  <input type="file" 
+		  name= "${occupation}">
+		</div>
+		<div class="file-path-wrapper">
+		  <input class="file-path validate" 
+		  type="text">
+		</div>
+	  </div>
+	</div>`;
+
+	$('#professionRow').append(professionList);
+
+	
+}
+				
+//============================================
+});// document on ready function closing tag 
---
+++ routes/checkboxes.js
@@ -0,0 +1,32 @@
+// Requiring our User model
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+    //GET ROUTE
+    //=========================================
+    // get request 
+    // listen to it via /professions
+    // request coming from the client is stored in the parameter req
+    // parameter res is there so we can send back a response to the client 
+    app.get('/professions', function(req, res){
+        // for the purpose of finding id and occupation, we are going to use findAll method
+        // we are using db to get access to the models
+        // name of the model we are going to use
+        // method we are going to use in this case find all 
+        // leave the object empty cuze we are not looking for anything specific 
+        db.Profession.findAll({})
+        //once it finsishes its task of finding all data
+        // we will store that data into a packet dbPost
+        .then(function(dbPost){
+            // now that we have access to that data in dbPost packet
+            // how do we display it to the user
+            // lets send it back to them in a json format
+            res.json(dbPost);
+            // ok now with that front end should know what to display because this information is whats retrieved in the result in ajax get request
+        })
+        
+    });
+};
+
