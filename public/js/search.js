2d784477349e7aceb466fd700b342510f7ea9ba8
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-03-02 02:17:34 -0500

    added a id to search icon and created a on click listener

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,9 @@
 $(document).ready(function(event){
-   
+    
+    //SEARCH ICON LISTENER
+    //=========================================
     console.log('im search.js')
-
+    $('#searchIcon').on('click', function(){
+        console.log('im working')
+    })
 });
